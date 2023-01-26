c5d069f15a814113e11d4ce1b7a3c01249ce0f3e
chaemin <w2114@e-mirim.hs.kr>
2023-01-26 22:29:08 +0900

    스크롤인터렉션1 완성

---
+++ section1/step1/index.js
@@ -0,0 +1,28 @@
+var progressBar = $('.progress .bar');
+var text = $('.progress .txt');
+$(function(){
+    $(window).scroll(function(){
+        getPercent();
+    })
+
+    function getPercent(){
+        var scrollHeight = $('.sec01').height();
+        var scrollRealHeight = scrollHeight - $(window).height();
+        var winScrollTop = $(window).scrollTop();
+        var scrollPercent = winScrollTop / scrollRealHeight * 100;
+        var textPercent = Math.round(scrollPercent);
+        render(textPercent, scrollPercent);
+    }
+
+    function render(textPercent, scrollPercent){
+        progressBar.css({
+            width: scrollPercent + '%'
+        });
+        text.text(textPercent + '%');
+    }
+
+    function init(){
+        getPercent();
+    }
+    init();
+});
